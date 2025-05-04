3097d47ac584d46eb233a65ac18653767107ec54
Celestine Agropah <129069651+datagirllab@users.noreply.github.com>
2025-05-04 23:26:00 +0000

    Create bronze_bulk_insert.sql
    Extract

---
+++ scripts/bronze/bronze_bulk_insert.sql
@@ -0,0 +1,83 @@
+/*
+Script: EXTRACT RAW DATA TO BRONZE
+Bulk insert raw data from source CSV files into the Bronze layer tables.
+  This script truncates existing data and uses BULK INSERT to load fresh data
+  from flat files into the corresponding Bronze schema tables.
+*/
+
+
+--BULK INSERT cust_info
+TRUNCATE TABLE bronze.crm_cust_info;
+BULK INSERT bronze.crm_cust_info
+FROM '<path_to_your_file>\cust_info.csv'
+WITH (
+     FIRSTROW = 2,
+	 FIELDTERMINATOR = ',',
+     TABLOCK
+	 );
+
+
+--BULK INSERT prd_info
+TRUNCATE TABLE bronze.crm_prd_info;
+BULK INSERT bronze.crm_prd_info
+FROM '<path_to_your_file>\prd_info.csv'
+WITH (
+     FIRSTROW = 2,
+	 FIELDTERMINATOR = ',',
+     TABLOCK
+	 );
+ SELECT COUNT (* )FROM bronze.crm_prd_info
+ SELECT * FROM bronze.crm_prd_info
+ 
+
+ --BULK INSERT sales_details
+ TRUNCATE TABLE bronze.crm_sales_details;
+ BULK INSERT bronze.crm_sales_details
+FROM '<path_to_your_file>\sales_details.csv'
+WITH (
+     FIRSTROW = 2,
+	 FIELDTERMINATOR = ',',
+     TABLOCK
+	 );
+SELECT * FROM bronze.crm_sales_details
+
+
+--BULK INSERT cust_az12
+TRUNCATE TABLE bronze.erp_cust_az12;
+BULK INSERT bronze.erp_cust_az12
+FROM '<path_to_your_file>\CUST_AZ12.csv'
+WITH (
+     FIRSTROW = 2,
+	 FIELDTERMINATOR = ',',
+     TABLOCK
+	 );
+SELECT COUNT (*) FROM bronze.erp_cust_az12
+SELECT * FROM bronze.erp_cust_az12
+
+
+--BULK INSERT loc_a101
+TRUNCATE TABLE bronze.erp_loc_a101;
+BULK INSERT bronze.erp_loc_a101
+FROM '<path_to_your_file>\LOC_A101.csv'
+WITH (
+     FIRSTROW = 2,
+	 FIELDTERMINATOR = ',',
+     TABLOCK
+	 );
+SELECT * FROM bronze.erp_loc_a101
+SELECT COUNT (*) FROM bronze.erp_loc_a101
+
+
+
+--BULK INSERT px_cat_g1v2
+TRUNCATE TABLE bronze.erp_px_cat_g1v2;
+BULK INSERT bronze.erp_px_cat_g1v2
+FROM '<path_to_your_file>\PX_CAT_G1V2.csv'
+WITH (
+     FIRSTROW = 2,
+	 FIELDTERMINATOR = ',',
+     TABLOCK
+	 );
+SELECT * FROM bronze.erp_px_cat_g1v2
+SELECT COUNT (*) FROM bronze.erp_px_cat_g1v2
+
